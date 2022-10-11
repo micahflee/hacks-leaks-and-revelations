8fcac6b16f4091f91a26fdb43c9ce518bba95630
Micah Lee <micah@micahflee.com>
2022-10-10 20:48:12 -0700

    Start updating chapter 9

--- chapter-9/homework-9-3.py
+++ chapter-9/homework-9-3.py
@@ -3,25 +3,17 @@ import os
 import csv
 import html
 
+
 @click.command()
 @click.argument("emailbuilder_csv_path")
 @click.argument("output_folder_path")
 def main(emailbuilder_csv_path, output_folder_path):
     """Make bulk emails in BlueLeaks easier to read"""
-    # Make sure output_folder_path exists and is a folder
+    print(f"Path to EmailBuilder.csv: {emailbuilder_csv_path}")
+    print(f"Output folder path: {output_folder_path}")
+
     os.makedirs(output_folder_path, exist_ok=True)
 
-    # A list of fields to include in the HTML output
-    important_keys = [
-        "EmailBuilderID",
-        "EmailFrom",
-        "EmailSubject",
-        "DateSent",
-        "Attachment1",
-        "SentEmailList",
-    ]
-
-    # Load the EmailBuidler.csv file and loop through its rows
     with open(emailbuilder_csv_path) as f:
         reader = csv.DictReader(f)
         for row in reader:
@@ -29,13 +21,26 @@ def main(emailbuilder_csv_path, output_folder_path):
                 f"{row['EmailBuilderID']}_{row['DateSent']}_{row['EmailSubject']}.html"
             )
             filename = filename.replace("/", "-")
+            filename = os.path.join(output_folder_path, filename)
+
+            # A list of fields to include in the HTML output
+            important_keys = [
+                "EmailBuilderID",
+                "EmailFrom",
+                "EmailSubject",
+                "DateSent",
+                "Attachment1",
+                "SentEmailList",
+            ]
 
             # Open the HTML file for writing
-            with open(os.path.join(output_folder_path, filename), "w") as f:
+            with open(filename, "w") as f:
                 f.write("<html><body>\n")
                 f.write("<ul>\n")
                 for key in important_keys:
-                    f.write(f"<li><strong>{key}:</strong> {html.escape(row[key])}</li>\n")
+                    f.write(
+                        f"<li><strong>{key}:</strong> {html.escape(row[key])}</li>\n"
+                    )
                 f.write("</ul>\n")
                 f.write(f"<div>{row['EmailBody']}</div>\n")
                 f.write("</body></html>\n")
