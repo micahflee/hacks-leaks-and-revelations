9ae97b4ef12d2b555da534fd3868e341e618f913
Micah Lee <micah@micahflee.com>
2022-12-06 21:23:14 -0800

    Tweak gab-scraper.py

--- appendix-b/gab-scraper.py
+++ appendix-b/gab-scraper.py
@@ -6,7 +6,7 @@ import click
 @click.argument("gab_username")
 def main(gab_username):
     """Download a user's posts from Gab"""
-    
+
     # Get information about the user
     r = httpx.get(f"https://gab.com/api/v1/account_by_username/{gab_username}")
     user_info = r.json()
@@ -16,18 +16,21 @@ def main(gab_username):
 
     # Display some user info
     click.echo(f"Display name: {user_info['display_name']}")
-    click.echo(f"{user_info['followers_count']:,} followers, {user_info['following_count']:,} following, {user_info['statuses_count']:,} posts")
+    click.echo(
+        f"{user_info['followers_count']:,} followers, {user_info['following_count']:,} following, {user_info['statuses_count']:,} posts"
+    )
     print()
 
     # Get this user's posts
     r = httpx.get(f"https://gab.com/api/v1/accounts/{user_info['id']}/statuses")
-    statuses = r.json()
-    for status in statuses:
-        if status["reblog"]:
-            print(f"repost @{status['reblog']['account']['username']}: {status['reblog']['created_at']}: {status['reblog']['content']}")
+    posts = r.json()
+    for post in posts:
+        if post["reblog"]:
+            print(
+                f"repost @{post['reblog']['account']['username']}: {post['reblog']['created_at']}: {post['reblog']['content']}"
+            )
         else:
-            print(f"{status['created_at']}: {status['content']}")
-    
+            print(f"{post['created_at']}: {post['content']}")
 
 
 if __name__ == "__main__":
