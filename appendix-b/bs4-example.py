85d0ef528ec5986dbad7b5aa37b657393c7401f4
Micah Lee <micah@micahflee.com>
2023-01-22 11:37:02 -0800

    Make bs4-example.py save output to a CSV

--- appendix-b/bs4-example.py
+++ appendix-b/bs4-example.py
@@ -1,22 +1,33 @@
+import csv
+import time
 import httpx
 from bs4 import BeautifulSoup
 
 
 def main():
-    for page_number in range(1, 6):
-        print(f"LOADING PAGE {page_number}")
-        r = httpx.get(f"https://news.ycombinator.com/?p={page_number}")
-        print("")
+    with open("output.csv", "w") as f:
+        writer = csv.DictWriter(f, fieldnames=["Title", "URL"])
+        writer.writeheader()
 
-        soup = BeautifulSoup(r.text, "html.parser")
-        for table_row in soup.find_all("tr", class_="athing"):
-            table_cells = table_row.find_all("td")
-            last_cell = table_cells[-1]
-            link = last_cell.find("a")
-            link_url = link.get("href")
-            print(link.text)
-            print(link_url)
+        for page_number in range(1, 6):
+            print(f"LOADING PAGE {page_number}")
+            r = httpx.get(f"https://news.ycombinator.com/?p={page_number}")
             print("")
+
+            soup = BeautifulSoup(r.text, "html.parser")
+            for table_row in soup.find_all("tr", class_="athing"):
+                table_cells = table_row.find_all("td")
+                last_cell = table_cells[-1]
+                link = last_cell.find("a")
+                link_url = link.get("href")
+
+                print(link.text)
+                print(link_url)
+                print("")
+
+                writer.writerow({"Title": link.text, "URL": link_url})
+
+            time.sleep(1)
 
 
 if __name__ == "__main__":
