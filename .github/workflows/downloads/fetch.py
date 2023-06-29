7ef2fc130706d5bf126f0666a4d85e07a75dfbdc
Micah Lee <micah@micahflee.com>
2023-06-29 15:59:18 -0700

    Track downloads (#1)
    Fix tracking downloads

--- .github/workflows/downloads/fetch.py
+++ .github/workflows/downloads/fetch.py
@@ -1,58 +1,82 @@
 import os
 import csv
+import json
 import requests
+import zipfile
+import io
 from datetime import datetime
 
 tag = "prerelease1"
 filename = "Introduction.pdf"
 
-# Fetch the download count from the GitHub API
-response = requests.get(
-    f"https://api.github.com/repos/micahflee/hacks-leaks-and-revelations/releases/tags/{tag}"
-)
-data = response.json()
-download_count = next(
-    asset["download_count"] for asset in data["assets"] if asset["name"] == filename
-)
 
-# Check if a previous "Track Downloads" workflow has been run
-try:
+def main():
+    # Fetch the download count from the GitHub API
+    url = f"https://api.github.com/repos/micahflee/hacks-leaks-and-revelations/releases/tags/{tag}"
+    response = requests.get(url)
+    data = response.json()
+    try:
+        download_count = next(
+            asset["download_count"]
+            for asset in data["assets"]
+            if asset["name"] == filename
+        )
+    except:
+        print(url)
+        print(json.dumps(data, indent=4))
+        raise Exception("Could not find download count")
+
+    # Check if a previous "Track Downloads" workflow has been run
     workflow_runs_response = requests.get(
-        "https://api.github.com/repos/micahflee/hacks-leaks-and-revelations/actions/workflows/downloads.yml/runs",
-        headers={"Authorization": f"token {os.getenv('GH_TOKEN')}"},
+        "https://api.github.com/repos/micahflee/hacks-leaks-and-revelations/actions/workflows/downloads.yml/runs"
     )
     workflow_runs_data = workflow_runs_response.json()
-
     if workflow_runs_data["total_count"] > 0:
-        # Download the 'downloads.csv' artifact from the last run
-        last_run_id = workflow_runs_data["workflow_runs"][0]["id"]
-        artifacts_response = requests.get(
-            f"https://api.github.com/repos/micahflee/hacks-leaks-and-revelations/actions/runs/{last_run_id}/artifacts",
-            headers={"Authorization": f"token {os.getenv('GH_TOKEN')}"},
-        )
-        artifacts_data = artifacts_response.json()
-        downloads_csv_artifact = next(
-            artifact
-            for artifact in artifacts_data["artifacts"]
-            if artifact["name"] == "downloads.csv"
-        )
-        downloads_csv_url = downloads_csv_artifact["archive_download_url"]
-        downloads_csv_response = requests.get(
-            downloads_csv_url,
-            headers={"Authorization": f"token {os.getenv('GH_TOKEN')}"},
-        )
-        with open("downloads.csv", "wb") as f:
-            f.write(downloads_csv_response.content)
-except Exception as e:
-    print(
-        f"Could not download 'downloads.csv' artifact from previous workflow run: {e}"
-    )
-    # Create a new 'downloads.csv' file if it could not be downloaded
-    with open("downloads.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["Date", "Downloads"])
+        # Find the artifacts URL of the last completed workflow run
+        artifacts_url = None
+        for workflow_run in workflow_runs_data["workflow_runs"]:
+            if workflow_run["status"] == "completed":
+                artifacts_url = workflow_run["artifacts_url"]
+                break
 
-# Open the spreadsheet and add the new download count
-with open("downloads.csv", "a", newline="") as f:
-    writer = csv.writer(f)
-    writer.writerow([datetime.now().date(), download_count])
+        if artifacts_url:
+            # Find the download-csv artifact URL
+            artifacts_response = requests.get(artifacts_url)
+            artifacts_data = artifacts_response.json()
+            print(artifacts_url)
+            print(json.dumps(artifacts_data, indent=4))
+            print()
+
+            downloads_csv_url = None
+            for artifact in artifacts_data["artifacts"]:
+                if artifact["name"] == "downloads-csv":
+                    downloads_csv_url = artifact["archive_download_url"]
+                    break
+
+            # If the downloads.csv artifact was found, download it
+            if downloads_csv_url:
+                downloads_csv_response = requests.get(
+                    downloads_csv_url,
+                    headers={"Authorization": f"token {os.getenv('GH_TOKEN')}"},
+                )
+                z = zipfile.ZipFile(io.BytesIO(downloads_csv_response.content))
+                z.extractall()
+            else:
+                print("No downloads.csv artifact found\n")
+        else:
+            print("No completed workflow runs found\n")
+    else:
+        print("No previous workflow runs found\n")
+
+    # Open the spreadsheet and add the new download count
+    with open("downloads.csv", "a", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow([datetime.now().date(), download_count])
+
+    # Display the contents of downloads.csv
+    print("downloads.csv:")
+    with open("downloads.csv", "r") as f:
+        print(f.read())
+
+
+main()
