edd4609b0e598b45662a71789e7fdf3f4ad219bf
Micah Lee <micah@micahflee.com>
2024-12-17 10:37:21 -0800

    Update workflow that keeps the Aleph docker-compose.yml file updated

--- .github/workflows/aleph/download_and_edit_aleph.py
+++ .github/workflows/aleph/download_and_edit_aleph.py
@@ -1,12 +1,45 @@
 import requests
+import re
 
-response = requests.get(
-    "https://raw.githubusercontent.com/alephdata/aleph/main/docker-compose.yml"
-)
-edited_content = (
-    "# https://github.com/alephdata/aleph/blob/main/docker-compose.yml\n"
-    + response.text.replace('- "~:/host"', '#- "~:/host"')
-)
 
-with open("temp_docker-compose.yml", "w") as f:
-    f.write(edited_content)
+def main():
+    response = requests.get(
+        "https://raw.githubusercontent.com/alephdata/aleph/main/docker-compose.yml"
+    )
+    content = response.text
+
+    # Remove the version line if it exists
+    content = re.sub(r"^version:.*\n", "", content, flags=re.MULTILINE)
+
+    # Find the default ALEPH_TAG version
+    match = re.search(
+        r"image: ghcr\.io/alephdata/aleph:\${ALEPH_TAG:-ALEPH_TAG:-([^}]+)}", content
+    )
+    if match:
+        default_version = match.group(1)
+    else:
+        raise ValueError("Default ALEPH_TAG version not found in the file")
+
+    # Replace ALEPH_TAG with the default version
+    content = re.sub(
+        r"image: ghcr\.io/alephdata/aleph:\${ALEPH_TAG:-ALEPH_TAG:-[^}]+}",
+        f"image: ghcr.io/alephdata/aleph:{default_version}",
+        content,
+    )
+    content = re.sub(
+        r"image: ghcr\.io/alephdata/aleph-ui-production:\${ALEPH_TAG:-ALEPH_TAG:-[^}]+}",
+        f"image: ghcr.io/alephdata/aleph-ui-production:{default_version}",
+        content,
+    )
+
+    edited_content = (
+        "# https://github.com/alephdata/aleph/blob/main/docker-compose.yml\n"
+        + content.replace('- "~:/host"', '#- "~:/host"')
+    )
+
+    with open("temp_docker-compose.yml", "w") as f:
+        f.write(edited_content)
+
+
+if __name__ == "__main__":
+    main()
